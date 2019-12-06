dd46c6cb442f7bb215c897e056fa0501fd3a8355
liyong003 <1577074107@qq.com>
2019-12-06 14:53:49 +0800

    评论页面

---
+++ pages/shop/evaluate/index.js
@@ -0,0 +1,109 @@
+// pages/shop/evaluate/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    flag: [0, 0],//态度和能力的分值组合，每一项值的范围在stardata里
+    startext: ['', ''],//分数的文字表述
+    stardata: [1, 2, 3, 4, 5],//分值
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+  // 五星评价事件
+  changeColor: function (e) {
+    var index = e.currentTarget.dataset.index;
+    var num = e.currentTarget.dataset.no;
+    var a = 'flag[' + index + ']';
+    var b = 'startext[' + index + ']';
+    var that = this;
+    if (num == 1) {
+      that.setData({
+        [a]: 1,
+        [b]: '非常不满意'
+      });
+    } else if (num == 2) {
+      that.setData({
+        [a]: 2,
+        [b]: '不满意'
+      });
+    } else if (num == 3) {
+      that.setData({
+        [a]: 3,
+        [b]: '一般'
+      });
+    } else if (num == 4) {
+      that.setData({
+        [a]: 4,
+        [b]: '满意'
+      });
+    } else if (num == 5) {
+      that.setData({
+        [a]: 5,
+        [b]: '非常满意'
+      });
+    }
+
+    console.log("a:", this.data.flag)
+  },
+  bindFormSubmit: function(e) {
+    const textArea = e.detail.value.textarea; //文本框值
+    const flagValue = this.data.flag // 态度和能力值，是个数组，要取出来处理
+ 
+  },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  
+})
